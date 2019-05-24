eed8f269a4a320d6b7c1347e4837fbcd3e24b517
Roman <romavaskov@yandex.ru>
2019-05-24 14:25:01 +0300

    first commit

---
+++ docs/js/navigation.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+	var navToggleButton = $('#navigation__button');
+	var navToggleIcon = $('.navigation__toggle .fa');
+	var navBlock = $('.navigation__list');
+	var navBlockOpen = 'navigation__list--open';
+	var navLink = $('.navigation__list a');
+
+	// Мобильная навигация
+	navToggleButton.on('click', function(e){
+		e.preventDefault();
+		navBlock.toggleClass(navBlockOpen);
+
+		if (navToggleButton.hasClass("active")) {
+      navToggleButton.removeClass("active");
+    } else{
+      navToggleButton.addClass("active");
+    }		
+	})
+
+	navLink.on('click', function(){
+		if ( navBlock.hasClass(navBlockOpen) ) {
+
+			if (navToggleButton.hasClass("active")) {
+      navToggleButton.removeClass("active");
+    	} else{
+      navToggleButton.addClass("active");
+    	}
+		}
+
+		navBlock.removeClass(navBlockOpen);		
+	})
+
+}); 
